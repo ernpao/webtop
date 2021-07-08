38c58d14b677ce71bef96330dc4b790449bdae26
Ernest-Epistola <ernestepistola@gmail.com>
2021-07-09 00:00:26 +0800

    Installed the esp32cam library and setup of demo app.

--- webtop_esp32/src/esp32_cam_demo/esp32_cam_demo.h
+++ webtop_esp32/src/esp32_cam_demo/esp32_cam_demo.h
@@ -3,6 +3,11 @@
 #define ESP32_CAM_DEMO_H
 
 #include <Arduino.h>
+#include <logging.h>
+
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32cam.h>
 
 void esp32_cam_demo_setup();
 void esp32_cam_demo_loop();
--- webtop_esp32/src/main.cpp
+++ webtop_esp32/src/main.cpp
@@ -2,12 +2,12 @@
 
 void setup()
 {
-  // esp32_cam_demo_setup();
-  esp32_camera_webtop_stream_setup();
+  esp32_cam_demo_setup();
+  // esp32_camera_webtop_stream_setup();
 }
 
 void loop()
 {
-  // esp32_cam_demo_loop();
-  esp32_camera_webtop_stream_loop();
+  esp32_cam_demo_loop();
+  // esp32_camera_webtop_stream_loop();
 }
