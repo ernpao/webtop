a10f34465d15110d0048444f10785c8ddebcc5b1
Ernest-Epistola <ernestepistola@gmail.com>
2021-07-28 18:44:26 +0800

    Changed WebtopClient -> WebtopWebAPI

--- webtop_flutter_apps/webtop_dashboard/lib/main.dart
+++ webtop_flutter_apps/webtop_dashboard/lib/main.dart
@@ -16,7 +16,7 @@ class WebtopDashboard extends StatelessWidget {
         backgroundColor: Colors.black,
         body: Center(
           child: BufferSourceImageStream(
-            interface: WebtopClient(
+            interface: WebtopWebAPI(
               host: "192.168.100.191",
               port: 6767,
               socketPort: 6868,
