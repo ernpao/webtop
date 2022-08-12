17463551911ec752cef87fbbd2e39d87fc177c10
Ernest-Epistola <ernestepistola@gmail.com>
2022-08-12 09:10:24 +0800

    Updated the ESP32 CAM Webtop project.

--- webtop_esp32/src/esp32_cam_webtop_stream/esp32_cam_webtop_stream.cpp
+++ webtop_esp32/src/esp32_cam_webtop_stream/esp32_cam_webtop_stream.cpp
@@ -1,6 +1,7 @@
 #include "esp32_cam_webtop_stream.h"
 
-WebtopClient webtop("192.168.100.191", 6767, 6868, "wNetworkSaBahay2.4GHz", "alotsipe");
+// WebtopClient webtop("192.168.100.191", 6767, 6868, "wNetworkSaBahay2.4GHz", "alotsipe");
+WebtopClient webtop("192.168.100.191", 6767, 6868, "ASUS AX3000 2.4 GHz", "ernestnet");
 
 static auto loRes = esp32cam::Resolution::find(320, 240);
 static auto hiRes = esp32cam::Resolution::find(800, 600);
@@ -21,14 +22,14 @@ void setupCamera()
         using namespace esp32cam;
         Config cfg;
         cfg.setPins(pins::AiThinker);
-        cfg.setResolution(hiRes);
-        cfg.setBufferCount(2); // Default is 2
-        cfg.setJpeg(100); // Default is 80
+        cfg.setResolution(loRes);
+        cfg.setBufferCount(5); // Default is 2
+        cfg.setJpeg(80);       // Default is 80
 
         bool ok = Camera.begin(cfg);
         Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
     }
-    esp32cam::Camera.changeResolution(maxRes);
+    esp32cam::Camera.changeResolution(loRes);
 }
 
 uint8_t *imageData;
@@ -43,6 +44,24 @@ void captureImage()
 
 void esp32_camera_webtop_stream_setup()
 {
+    // Setup camera flashlight
+
+    const int freq = 5000;
+    const int ledChannel = 0;
+    const int resolution = 8;
+    const int flashPin = 4;
+    // pinMode(flashPin, OUTPUT);
+    // digitalWrite(flashPin, HIGH);
+
+    ledcSetup(ledChannel, freq, resolution);
+    delay(100);
+    // attach the channel to the GPIO to be controlled
+    ledcAttachPin(flashPin, ledChannel);
+    delay(100);
+    int dutyCycle = 4;
+    ledcWrite(ledChannel, dutyCycle);
+    delay(100);
+
     initLogging();
     webtop.printInfo();
     webtop.connectToWiFi();
@@ -55,7 +74,7 @@ void esp32_camera_webtop_stream_loop()
 {
     static unsigned long lastCaptureTime = 0;
     unsigned long currentTime = millis();
-    unsigned long sendInterval = 200;
+    unsigned long sendInterval = 100;
     if (currentTime - lastCaptureTime > sendInterval)
     {
         captureImage();
--- webtop_esp32/src/main.cpp
+++ webtop_esp32/src/main.cpp
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/*
+  ESP32 CAM Uploading: 
+    - Pin D0 must be connected to GND during upload. Disconnect D0 from GND and press RST after upload.
+    - There is no need to hold the RST/FLASH buttons during upload. When upload begins, simply press the RST button to begin flashing.
+*/
+
 void setup()
 {
   // esp32_cam_demo_setup();
-  // esp32_camera_webtop_stream_setup();
+  esp32_camera_webtop_stream_setup();
   // esp32_homespan_setup();
-  esp32_imu_webtop_stream_setup();
+  // esp32_imu_webtop_stream_setup();
 }
 
 void loop()
 {
   // esp32_cam_demo_loop();
-  // esp32_camera_webtop_stream_loop();
+  esp32_camera_webtop_stream_loop();
   // esp32_homespan_loop();
-  esp32_imu_webtop_stream_loop();
+  // esp32_imu_webtop_stream_loop();
 }
